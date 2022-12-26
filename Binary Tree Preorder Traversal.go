b801b578fe3de44aae6000139f5b123eabe65722
wawilow <wawilow@protonmail.com>
2022-12-26 13:12:44 +0300

    886 problem

---
+++ Binary Tree Preorder Traversal.go
@@ -0,0 +1,23 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func abs(ans *[]int, tree TreeNode) {
+	*ans = append(*ans, tree.Val)
+	if tree.Left != nil {
+		abs(ans, *tree.Left)
+	}
+	if tree.Right != nil {
+		abs(ans, *tree.Right)
+	}
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	ans := []int{}
+	abs(&ans, *root)
+	return ans
+}
